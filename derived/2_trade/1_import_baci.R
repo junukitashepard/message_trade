7bbff79b659962960d280fa4bf6c0bf386d17e4e
Shepard Jun <shepard@iiasa.ac.at>
2019-06-13 16:12:57 +0200

    Update BACI import with IEA heating values (NCV)

--- derived/2_trade/1_import_baci.R
+++ derived/2_trade/1_import_baci.R
@@ -19,136 +19,113 @@ raw <-      paste0(wd, "raw/")
 energyhs4 <- read.csv(file.path(raw, "BACI/energy2hs4.csv"), stringsAsFactors = F)
 energyhs4$hs4 <- as.character(energyhs4$hs4)
 energyhs4 <- subset(energyhs4, !is.na(hs4))
+energyhs4$hs6 <- as.character(energyhs4$hs6)
 
-# Import country names
+# Import country crosswalk
 countries <- read.csv(file.path(raw, "ConversionTables/web_countries.csv"), stringsAsFactors = F)
-countries$X <- NULL
-countries <- subset(countries, !is.na(subregion))
+countries <- countries[c('iso.country', 'web.country', 'baci.country')]
 
-# Import specific energy
-spec_energy <- read.csv(file.path(raw, "ConversionTables/energy_content_of_fuels/fuel_energy_content.csv"),
-                        stringsAsFactors = F)
-names(spec_energy) <- c('energy_type', 'specific_energy', 'region')
+# Import NCV values from IEA
+ncv <- readRDS(file.path(temp, 'ncv_convert.rds'))
 
-# Link specific energy to countries dataframe
-se_country <- subset(spec_energy, nchar(region) == 3) # country-specific
-se_region <- subset(spec_energy, nchar(region) > 3) # region-specific
-
-se.df <- data.frame(web.country = character(0), iso.country = character(0), baci.country = integer(0),
-                     region = character(0), subregion = character(0),
-                     specific_energy = numeric(0), energy = character(0))
-
-for (e in c('BIO', 'CRU', 'COAL', 'NG', 'PET', 'NUC')) {
-  print(paste0("Linking: ", e))
-  df.c <- subset(se_country, energy_type == e)[c('specific_energy', 'region')]
-  df.r <- subset(se_region, energy_type == e)[c('specific_energy', 'region')]
-
-  if (nrow(df.c) == 1 & df.c$region == 'all') {
-    df.out <- countries
-    df.out$specific_energy <- df.c$specific_energy[1]
-  } else {
-    df.out <- left_join(countries, df.c, by = c('iso.country' = 'region'))
-    df.out$specific_energy[is.na(df.out$specific_energy)] <- 0
-    for (r in unique(df.r$region)) {
-      print(r)
-      df.out$specific_energy[df.out$specific_energy == 0 & df.out$subregion == r] <-
-        df.r$specific_energy[df.r$region == r]
-    }
-  }
-
-  assert('!is.na(df.out$specific_energy)')
-  df.out$energy <- e
-  names(df.out) <- c('web.country', 'iso.country', 'baci.country', 'region', 'subregion', 'specific_energy', 'energy')
-  se.df <- rbind(se.df, df.out)
-}
-
-se.df <- se.df[c('baci.country', 'energy', 'specific_energy')] # keep relevant variables
-rm(list = c('df.c', 'df.out', 'df.r', 'se_country', 'se_region', 'spec_energy'))
-
+ncv.r <- dplyr::group_by(ncv, product, energy) %>%
+  dplyr::summarise(ncv = mean(ncv, na.rm = T)) # Where IEA missing country data, use overall average
+names(ncv.r) <- c('product', 'energy', 'global.ncv')
 
 # Function "trade2physical": convert quantity (t) to energy flows using region-specific specific energy values
-trade2physical <- function(year) {
+trade2physical <- function(y) {
 
-  print(paste0("Converting trade data, year = ", year))
-
-  # Import trade (BACI) data from SQL database
-  sql_statement <- paste0("SELECT * FROM BACI_TRADE.BACI_", year, " ",
-                          "WHERE LEFT(HS6, 2) = '27' OR
-                          LEFT(HS6,2) = '38' OR
-                          LEFT(HS6,2) = '28'")
-  quiet(
+  environment(assert) <- environment()
+  
+  print(paste0('Importing: ', y))
+  
+  sql_statement <- paste0("SELECT * FROM BACI_TRADE.BACI_", y, " ",
+                      "WHERE LEFT(BACI_TRADE.BACI_", y, ".hs6, 2) = '27' OR ",
+                      "LEFT(BACI_TRADE.BACI_", y, ".hs6, 2) = '38' OR ",
+                      "LEFT(BACI_TRADE.BACI_", y, ".hs6, 2) = '28' OR ",
+                      "LEFT(BACI_TRADE.BACI_", y, ".hs6, 2) = '22'")
+  
   import_sql(statement = sql_statement,
              user = 'root',
              password = 'SEAmonst3r!',
              dbname = 'BACI_TRADE',
-             outdf = 'trade'
-  ))
+             outdf = 'baci')
+  
+  baci$hs6 <- as.character(baci$hs6)
+  assert('nchar(baci$hs6) == 6')
+  
+  # Link BACI to crosswalks
+  baci <- left_join(baci, energyhs4, by = c('hs6'))
+  baci <- subset(baci, !is.na(energy))
+  baci <- left_join(baci, countries, by = c('i' = 'baci.country')) # exporter only
 
-  trade$hs4 <- substr(trade$hs6, 1, 4)
-  trade <- left_join(trade, energyhs4, by = c('hs4'))
-  trade <- subset(trade, !is.na(energy))
+  # Convert q to energy (TJ) using NCV from IEA 
+  baci <- left_join(baci, ncv, by = c('iso.country', 'hs6_desc' = 'product', 'energy'))
+  baci <- left_join(baci, ncv.r, by = c('hs6_desc' = 'product', 'energy' = 'energy'))
 
-  # Link specific energy by exporting country
-  trade <- left_join(trade, se.df, by = c('i' = 'baci.country', 'energy' = 'energy'))
-  environment(assert) <- environment(isid) <- environment()
-  trade <- subset(trade, grepl('ELEC', energy) == F)
+  baci$global.ncv[baci$energy == 'NUC'] <- 500 # Uranium is a global heating value
+  
+  baci$ncv[is.na(baci$ncv)] <- baci$global.ncv[is.na(baci$ncv)]
+  baci$global.ncv <- NULL
 
-  trade <- subset(trade, i != 652 & j != 652) # country 652 does not exist
-
-  assert('!is.na(trade$specific_energy)')
+  check <- subset(baci, is.na(ncv))
+  assert('check$energy == "ELEC" | check$energy == "HYD"')
+  
+  baci$q_e <- baci$ncv * baci$q
 
   # Collapse to i-j-year-energy level
-  trade <- dplyr::group_by(trade, i, j, t, energy, specific_energy) %>%
-           dplyr::summarise(v = sum(v, na.rm = T),
-                            q = sum(q, na.rm = T))
-
-  trade$q_e <- trade$q * trade$specific_energy #TJ energy flow
+  baci <- dplyr::group_by(baci, i, j, t, energy) %>%
+    dplyr::summarise(v = sum(v, na.rm = T),
+                     q = sum(q, na.rm = T),
+                     q_e = sum(q_e, na.rm = T))
+  names(baci) <- c('i', 'j', 'year', 'energy', 'v', 'q', 'q_e')
 
   # Add country names
-  trade <- left_join(trade, countries[c('baci.country', 'iso.country')], by = c('i' = 'baci.country'))
-  trade <- left_join(trade, countries[c('baci.country', 'iso.country')], by = c('j' = 'baci.country'))
-  names(trade)[9:10] <- c('iso.i', 'iso.j')
-
-  assign(paste0('trade.', year), trade, envir = parent.frame())
+  baci <- left_join(baci, countries[c('baci.country', 'iso.country')], by = c('i' = 'baci.country'))
+  baci <- left_join(baci, countries[c('baci.country', 'iso.country')], by = c('j' = 'baci.country'))
+  names(baci)[names(baci) == 'iso.country.x'] <- 'iso.i'
+  names(baci)[names(baci) == 'iso.country.y'] <- 'iso.j'
+  
+  assign(paste0('trade.', y), baci, envir = parent.frame())
 }
 
 # Function "collapsebytype": collapse to imports and exports for IEA data validation
 collapsebytype <- function(year) {
   print(paste0("Collapsing data, year = ", year))
-
+  
   assign('df', get(paste0('trade.', year)))
-
+  
   # Collapse to imports and exports separately
-  assign('imports', dplyr::group_by(df, j, iso.j, t, energy) %>%
-                            dplyr::summarise(v = sum(v, na.rm = T),
-                                             q= sum(q, na.rm = T),
-                                             q_e = sum(q_e, na.rm = T)))
-
-  assign('exports', dplyr::group_by(df, i, iso.i, t, energy) %>%
-                            dplyr::summarise(v = sum(v, na.rm = T),
-                                             q = sum(q, na.rm = T),
-                                             q_e = sum(q_e, na.rm = T)))
+  assign('imports', dplyr::group_by(df, j, iso.j, year, energy) %>%
+           dplyr::summarise(v = sum(v, na.rm = T),
+                            q= sum(q, na.rm = T),
+                            q_e = sum(q_e, na.rm = T)))
+  
+  assign('exports', dplyr::group_by(df, i, iso.i, year, energy) %>%
+           dplyr::summarise(v = sum(v, na.rm = T),
+                            q = sum(q, na.rm = T),
+                            q_e = sum(q_e, na.rm = T)))
   names(exports) <- names(imports) <- c('baci.country', 'iso.country', 'year', 'energy', 'v', 'q', 'q_e')
   exports$trade <- 'exports'
   imports$trade <- 'imports'
-
+  
   assign(paste0('trade_bytype.', year),
          rbind(as.data.frame(exports), as.data.frame(imports)),
          envir = parent.frame())
-
+  
 }
 
 # Run programs by year
-trade2physical(year = 1995)
-collapsebytype(year = 1995)
+trade2physical(1995)
+collapsebytype(1995)
 
 trade <- trade.1995
 trade_bytype <- trade_bytype.1995
 
 for (y in 1996:2014) {
-  trade2physical(year = y)
-  collapsebytype(year = y)
-
+  trade2physical(y)
+  collapsebytype(y)
+  
   assign('trade', rbind(as.data.frame(trade), as.data.frame(get(paste0('trade.', y)))))
   assign('trade_bytype', rbind(as.data.frame(trade_bytype), as.data.frame(get(paste0('trade_bytype.', y)))))
 }
@@ -156,4 +133,6 @@ for (y in 1996:2014) {
 # Write files
 saveRDS(trade, file.path(temp, 'trade.rds'))
 saveRDS(trade_bytype, file.path(temp, 'trade_bytype.rds'))
+
+
 
